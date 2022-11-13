9e3cc946cba9f8585879daa9bbcd093d69dc2dd7
Documenter.jl <documenter@juliadocs.github.io>
2022-11-13 08:28:56 +0000

    build based on acda682

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LieTheory","category":"page"},{"location":"#LieTheory","page":"Home","title":"LieTheory","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LieTheory.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LieTheory]","category":"page"}]
+}
